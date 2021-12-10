b31d2da1031296388d97792dfc6c8846e547a07c
neil040 <neil@neiloconnor.com>
2021-12-10 16:38:48 +0000

    day 10 part 2

--- cmd/day10/main.go
+++ cmd/day10/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"log"
 	"os"
+	"sort"
 )
 
 type runeStack struct {
@@ -14,18 +16,21 @@ func (rs *runeStack) push(r rune) {
 	rs.stack = append(rs.stack, r)
 }
 
-func (rs *runeStack) pop() rune {
+func (rs *runeStack) pop() (rune, error) {
+	if len(rs.stack) == 0 {
+		return ' ', errors.New("pop from empty stack")
+	}
 	r := rs.stack[len(rs.stack)-1]
-	rs.stack = rs.stack[0:]
-	return r
+	rs.stack = rs.stack[0 : len(rs.stack)-1]
+	return r, nil
 }
 
 func main() {
-	day10_1()
+	day10()
 }
 
-func day10_1() {
-	f, err := os.Open("input_test.txt")
+func day10() {
+	f, err := os.Open("input.txt")
 	if err != nil {
 		log.Fatal("Couldn't open input")
 	}
@@ -33,28 +38,46 @@ func day10_1() {
 	scanner := bufio.NewScanner(f)
 	scanner.Split(bufio.ScanLines)
 	matchingRunes := map[rune]rune{']': '[', ')': '(', '}': '{', '>': '<'}
-	scores := map[rune]int{')': 3, ']': 57, '}': 1197, '>': 25137}
+	corruptScores := map[rune]int{')': 3, ']': 57, '}': 1197, '>': 25137}
+	incompleteScores := map[rune]int{'(': 1, '[': 2, '{': 3, '<': 4}
 
-	total := 0
+	totalCorrupt := 0
+	totalIncomplete := make([]int, 0)
 	for scanner.Scan() {
-		inChunk := map[rune]int{'[': 0, '(': 0, '{': 0, '<': 0}
+		openingRuneStack := runeStack{}
 		line := scanner.Text()
-		for i, r := range line {
+		corrupted := false
+		for _, r := range line {
 			switch {
 			case r == '[' || r == '(' || r == '{' || r == '<':
-				inChunk[r]++
+				openingRuneStack.push(r)
 			case r == ']' || r == ')' || r == '}' || r == '>':
 				openingRune := matchingRunes[r]
-				if inChunk[openingRune] > 0 {
-					inChunk[openingRune]--
-				} else {
+				mustMatch, _ := openingRuneStack.pop()
+				if openingRune != mustMatch {
 					// unmatched closing rune
-					log.Printf("Found unmatched %c in %s at position %d\n", r, line, i)
-					total += scores[r]
+					totalCorrupt += corruptScores[r]
+					corrupted = true
 				}
 			}
 		}
+		if !corrupted { //incomplete
+			score := 0
+			for {
+				r, err := openingRuneStack.pop()
+				if err != nil {
+					break
+				}
+				score = score * 5
+				score += incompleteScores[r]
+			}
+			totalIncomplete = append(totalIncomplete, score)
+		}
 	}
 
-	log.Println("Total score for corrupted lines is", total)
+	// sort incomplete scores and find median
+	sort.Ints(totalIncomplete)
+
+	log.Println("Total score for corrupted lines is", totalCorrupt)
+	log.Println("Total score for incomplete lines is", totalIncomplete[len(totalIncomplete)/2])
 }
